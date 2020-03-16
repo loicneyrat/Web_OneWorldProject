1653258fe3784184b5c981851e74cdfdc85b531c
loicneyrat <loicneyrat@hotmail.fr>
2020-03-16 22:25:14 +0100

    express added to node_modules directory

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 let express = require('express');
 let app = express();
 let mustache = require('mustache-express');
+let resources = require('resourcesServer.js');
 
 let model = require('./model/model.js');
+
 
 app.engine('html', mustache());
 app.set('view engine', 'html');
 app.set('views', './views');
+
+
+app.use('/styles', resources);
+
 
 app.get('/', (req, res) => {
     res.render('index.html');
