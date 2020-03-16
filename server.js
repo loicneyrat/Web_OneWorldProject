7581abf2130fa76f469c3274e1bb34ce40a866c2
loicneyrat <loicneyrat@hotmail.fr>
2020-03-16 22:15:44 +0100

    Creation and first implementation of the main server.

---
+++ server.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let app = express();
+let mustache = require('mustache-express');
+
+let model = require('./model/model.js');
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.get('/#', (req, res) => {
+    res.redirect('/');
+});
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+    res.send("404 Not Found");
+    next();
+});
+
