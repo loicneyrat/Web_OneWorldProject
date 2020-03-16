627bd9bceb354cd69144f7c0914f7dd83a74389f
robin <robinseverac@hotmail.fr>
2020-03-16 23:17:53 +0100

    model functions modified to include the changes method

--- model/model.js
+++ model/model.js
@@ -4,7 +4,7 @@ var db = new sqlite('database.sqlite');
 
 db.prepare('CREATE TABLE users (email VARCHAR2(30) PRIMARY KEY, username VARCHAR2(20) UNIQUE, password VARCHAR2(50)), status VARCHAR2(20)').run();
 
-db.prepare('CREATE TABLE projects (projectId INTEGER(7) PRIMARY KEY, name VARCHAR2(60), description VARCHAR2(1000), creator VARCHAR2(30) REFERENCES users)').run();
+db.prepare('CREATE TABLE projects (projectId INTEGER(7) PRIMARY KEY AUTOINCREMENT, name VARCHAR2(60), description VARCHAR2(1000), creator VARCHAR2(30) REFERENCES users)').run();
 
 db.prepare('CREATE TABLE projectMembers(projectId INTEGER(7) REFERENCES projects ON DELETE CASCADE, user VARCHAR2(30) REFERENCES users ON DELETE CASCADE, status VARCHAR2(15), PRIMARY KEY(projectId, user)').run();
 
@@ -41,9 +41,8 @@ exports.deleteUser = function(email) {
 }
 
 exports.createProject = function(name, description, creator) {
-    let projectId = db.prepare('SELECT count(projectId) FROM projects').get();
     let insert = db.prepare('INSERT INTO project VALUES (?, ?, ?, ?)');
-    insert.run([projectId, name, description, creator]);
+    let projectId = insert.run([projectId, name, description, creator]).lastInsertRowId;
     return projectId;
 }
 
@@ -52,16 +51,16 @@ exports.updateProject = function(projectId, name, description, creator) {
     if (check === false) return false;
 
     let update = db.prepare('UPDATE projects SET name=?, description=?, creator=? WHERE projectId=?');
-    update.run([name, description, creator, projectId]);
-    return true;
+    let result = update.run([name, description, creator, projectId]).changes;
+    return result == 1;
 }
 
 exports.deleteProject = function(projectId) {
     let check = sqlCheck(projectId, projects);
     if (check === false) return false;
 
-    db.prepare('DELETE FROM projects WHERE projectId=?').run([projectId]);
-    return true;
+    let result = db.prepare('DELETE FROM projects WHERE projectId=?').run([projectId]).changes;
+    return result == 1;
 }
 
 exports.addMember = function(projectId, user, status) {
@@ -81,8 +80,8 @@ exports.updateMember = function(projectId, user, status) {
     if(check === undefined) return false;
 
     let update = db.prepare('UPDATE projectMembers SET user=?, status=? WHERE projectId=?');
-    update.run([user, status, projectId]);
-    return true;
+    let result = update.run([user, status, projectId]).changes;
+    return result == 1;
 }
 
 exports.deleteMember = function(projectId, user) {
@@ -90,8 +89,8 @@ exports.deleteMember = function(projectId, user) {
     if(check === undefined) return false;
 
     let query = db.prepare('DELETE FROM projectMembers WHERE projectId=? AND user=?');
-    query.run([projectId, user]);
-    return true;
+    let result = query.run([projectId, user]).changes;
+    return result == 1;
 }
 
 exports.addKeyword = function(projectId, keyword) {
@@ -111,8 +110,8 @@ exports.deleteKeyword = function(projectId, keyword) {
     let check = sqlCheck(projectId, projectKeywords);
     if (check === false) return false;
 
-    db.prepare('DELETE FROM projectKeywords WHERE projectId=? AND keyword=?').run([projectId, keyword]);
-    return true;
+    db.prepare('DELETE FROM projectKeywords WHERE projectId=? AND keyword=?');let result = run([projectId, keyword]).changes;
+    return result == 1;
 }
 
 exports.addEvent = function(projectId, event) {
@@ -133,16 +132,17 @@ exports.updateEvent = function(projectId, previousEvent, newEvent) {
     if (check === undefined) return false;
 
     let update = db.prepare('UPDATE projectEvents SET event=? WHERE projectId=? AND event=?');
-    update.run([newEvent, projectId, previousEvent]);
-    return true;
+    let result = update.run([newEvent, projectId, previousEvent]).changes;
+    return result == 1;
 }
 
 exports.deleteEvent = function(projectId, event) {
     let check = db.prepare('SELECT * FROM projectEvents WHERE projectId=? AND event=?').get([projectId, event]);
     if (check === undefined) return false;
 
-    db.prepare('DELETE FROM projectEvents WHERE projectId=? AND event=?').run([projectId, event]);
-    return true;
+    let toDelete = db.prepare('DELETE FROM projectEvents WHERE projectId=? AND event=?');
+    let result = toDelete.run([projectId, event]).changes;
+    return result == 1;
 }
 
 exports.resetDatabase = function() {
@@ -156,7 +156,7 @@ exports.resetDatabase = function() {
 
     db.prepare('CREATE TABLE users (email VARCHAR2(30) PRIMARY KEY, username VARCHAR2(20) UNIQUE, password VARCHAR2(50)), status VARCHAR2(20)').run();
 
-    db.prepare('CREATE TABLE projects (projectId INTEGER(7) PRIMARY KEY, name VARCHAR2(60), description VARCHAR2(1000), creator VARCHAR2(30) REFERENCES users)').run();
+    db.prepare('CREATE TABLE projects (projectId INTEGER(7) PRIMARY KEY AUTOINCREMENT, name VARCHAR2(60), description VARCHAR2(1000), creator VARCHAR2(30) REFERENCES users)').run();
 
     db.prepare('CREATE TABLE projectMembers(projectId INTEGER(7) REFERENCES projects ON DELETE CASCADE, user VARCHAR2(30) REFERENCES users ON DELETE CASCADE, status VARCHAR2(15), PRIMARY KEY(projectId, user)').run();
 
