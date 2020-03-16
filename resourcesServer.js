807b2b2b2672760c39ecee4bf1c530eaa2fcc713
loicneyrat <loicneyrat@hotmail.fr>
2020-03-17 00:34:20 +0100

    Routes for background image and tab icon added in resourcesServer.js

--- resourcesServer.js
+++ resourcesServer.js
@@ -1,10 +1,19 @@
 let express = require('express');
 let app = express();
 
-app.get('/styles.css', (req, res) => {
-    res.sendFile(__dirname + '/style.css');
+app.get('/style.css', (req, res) => {
+    res.sendFile(__dirname + '/styles/style.css');
 });
 
+app.get('/112868.jpg', (req, res) => {
+    res.set('Content-Type', 'image/jpg');
+    res.sendFile(__dirname + '/resources/112868.jpg');
+});
+
+app.get('/earth-icon.png', (req, res) => {
+    res.set('Content-Type', 'image/png');
+    res.sendFile(__dirname + '/resources/earth-icon.png');
+});
 
 
 
