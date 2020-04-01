59fde63bdd1131041a5a3dc66b843244a8fd63cc
robin <robinseverac@hotmail.fr>
2020-04-01 14:13:32 +0200

    categoriesHandler function implemented and model adapted

--- model/categoriesHandler.js
+++ model/categoriesHandler.js
@@ -1,20 +1,23 @@
 exports.addCategory = function(projectId, category) {
-    let check = sqlCheck(projectId, projects);
-    if (check === undefined) return false;
+   
+   if(isAlreadyPresent(projectId, category)) return false;
 
-    check = db.prepare('SELECT * FROM projectCategories WHERE projectID=? AND category=?').get([projectId, category]);
-    if (check !== undefined) return false;
-
-
-    let insert = db.prepare('INSERT INTO projectCategories VALUES (?, ?)');
-    let result = insert.run([projectId, category]).changes;
-    return result === 1;
+   let insert = db.prepare('INSERT INTO projectCategories VALUES (?, ?)');
+   let result = insert.run([projectId, category]);
+   return result.changes === 1;
  }
+ 
 
  exports.removeCategory = function(projectId, category) {
-    let check = sqlCheck(projectId, projects);
-    if (check === undefined) return false;
+   let check = sqlCheck(projectId, projects);
+   if (check === undefined) return false;
 
-    let result = db.prepare('DELETE FROM projectKeywords WHERE projectId=? AND category=?').run([projectId, category]).changes;
-    return result === 1;
+   let result = db.prepare('DELETE FROM projectKeywords WHERE projectId=? AND category=?').run([projectId, category]);
+   return result.changes === 1;
+ }
+
+
+ function isAlreadyPresent(projectId, category) {
+   let check = db.prepare('SELECT * FROM projectCategories WHERE projectID=? AND category=?').get([projectId, category]);
+   return check !== undefined;
  }
--- model/model.js
+++ model/model.js
@@ -171,24 +171,11 @@ exports.removeEvent = function(projectId, event) {
  */
 
  exports.addCategory = function(projectId, category) {
-    let check = sqlCheck(projectId, projects);
-    if (check === undefined) return false;
-
-    check = db.prepare('SELECT * FROM projectCategories WHERE projectID=? AND category=?').get([projectId, category]);
-    if (check !== undefined) return false;
-
-
-    let insert = db.prepare('INSERT INTO projectCategories VALUES (?, ?)');
-    let result = insert.run([projectId, category]).changes;
-    return result === 1;
+    return categoriesHandler.addCategory(projectId, category);
  }
 
  exports.removeCategory = function(projectId, category) {
-    let check = sqlCheck(projectId, projects);
-    if (check === undefined) return false;
-
-    let result = db.prepare('DELETE FROM projectKeywords WHERE projectId=? AND category=?').run([projectId, category]).changes;
-    return result === 1;
+    return categoriesHandler.removeCategory(projectId, category);
  }
 
 
