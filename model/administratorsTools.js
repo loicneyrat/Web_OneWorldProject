669268a3bc9f4a19205fc13a0da7c90aa8f68b36
robin <robinseverac@hotmail.fr>
2020-04-01 13:42:52 +0200

    administratorsTools functions implemented and model adapted

--- model/administratorsTools.js
+++ model/administratorsTools.js
@@ -1,10 +1,7 @@
 exports.updateUserStatus = function (email, status) {
-    let check = sqlCheck(email, users);
-    if (check == false) return false;
-
     let update = db.prepare('UPDATE users SET status=? WHERE email=?');
-    let result = update.run([status, email]).changes;
-    return result == 1;
+    let result = update.run([status, email]);
+    return result.changes == 1;
 }
 
 exports.getUsersList = function() {
