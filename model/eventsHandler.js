40f5401dae91c594505e163aa9cc823c31c5ebcd
robin <robinseverac@hotmail.fr>
2020-04-01 14:09:23 +0200

    eventsHandler functions implemented and model adapted

--- model/eventsHandler.js
+++ model/eventsHandler.js
@@ -1,30 +1,30 @@
 exports.addEvent = function(projectId, event, date) {
-    let check = sqlCheck(projectId, projects);
-    if (check === false) return false;
-
-    check = db.prepare('SELECT * FROM projectEvents WHERE projectID=? AND event=?').get([projectId, event]);
-    if (check !== undefined) return false;
-
+    
+    if(eventAlreadyExist(projectId, event, date)) return false;
 
     let insert = db.prepare('INSERT INTO projectEvents VALUES (?, ?, ?)');
-    let result = insert.run([projectId, event, date]).changes;
-    return result === 1;
+    let result = insert.run([projectId, event, date]);
+    return result.changes === 1;
 }
+
 
 exports.updateEvent = function(projectId, previousEvent, newEvent) {
-    let check = db.prepare('SELECT * FROM projectEvents WHERE projectId=? AND event=?').get([projectId, previousEvent]);
-    if (check === undefined) return false;
-
+    
     let update = db.prepare('UPDATE projectEvents SET event=? WHERE projectId=? AND event=?');
-    let result = update.run([newEvent, projectId, previousEvent]).changes;
-    return result === 1;
+    let result = update.run([newEvent, projectId, previousEvent]);
+    return result.changes === 1;
 }
 
-exports.removeEvent = function(projectId, event) {
-    let check = db.prepare('SELECT * FROM projectEvents WHERE projectId=? AND event=?').get([projectId, event]);
-    if (check === undefined) return false;
 
+exports.removeEvent = function(projectId, event) {
+    
     let toDelete = db.prepare('DELETE FROM projectEvents WHERE projectId=? AND event=?');
-    let result = toDelete.run([projectId, event]).changes;
-    return result === 1;
+    let result = toDelete.run([projectId, event]);
+    return result.changes === 1;
+}
+
+
+function eventAlreadyExist(projectId, event, date) {
+    let check = db.prepare('SELECT * FROM projectEvents WHERE projectID=? AND event=? AND date=?').get([projectId, event, date]);
+    return check !== undefined;
 }
--- model/model.js
+++ model/model.js
@@ -3,7 +3,7 @@ var usersHandler = require('usersHandler.js');
 var projectsHandler = require('projectsHandler.js');
 var administratorsTools = require('administratorsTools.js');
 var categoriesHandler = require('categoriesHandler.js');
-var eventshandler = require('eventsHandler.js');
+var eventsHandler = require('eventsHandler.js');
 var keywordsHandler = require('keywordsHandler.js');
 var db = new sqlite('database.sqlite');
 
@@ -152,34 +152,15 @@ exports.removeKeyword = function(projectId, keyword) {
  */
 
 exports.addEvent = function(projectId, event, date) {
-    let check = sqlCheck(projectId, projects);
-    if (check === false) return false;
-
-    check = db.prepare('SELECT * FROM projectEvents WHERE projectID=? AND event=?').get([projectId, event]);
-    if (check !== undefined) return false;
-
-
-    let insert = db.prepare('INSERT INTO projectEvents VALUES (?, ?, ?)');
-    let result = insert.run([projectId, event, date]).changes;
-    return result === 1;
+    return eventsHandler.addEvent(projectId, event, date);
 }
 
 exports.updateEvent = function(projectId, previousEvent, newEvent) {
-    let check = db.prepare('SELECT * FROM projectEvents WHERE projectId=? AND event=?').get([projectId, previousEvent]);
-    if (check === undefined) return false;
-
-    let update = db.prepare('UPDATE projectEvents SET event=? WHERE projectId=? AND event=?');
-    let result = update.run([newEvent, projectId, previousEvent]).changes;
-    return result === 1;
+    return eventsHandler.updateEvent(projectId, previousEvent, newEvent);
 }
 
 exports.removeEvent = function(projectId, event) {
-    let check = db.prepare('SELECT * FROM projectEvents WHERE projectId=? AND event=?').get([projectId, event]);
-    if (check === undefined) return false;
-
-    let toDelete = db.prepare('DELETE FROM projectEvents WHERE projectId=? AND event=?');
-    let result = toDelete.run([projectId, event]).changes;
-    return result === 1;
+    return eventsHandler.removeEvent(projectId, event);
 }
 
 
