47ef769925787882e29ab9e97f541604ffcf5185
loicneyrat <loicneyrat@hotmail.fr>
2020-04-09 19:56:32 +0200

    Checked

--- model/keywordsHandler.js
+++ model/keywordsHandler.js
@@ -3,23 +3,18 @@ var db = new sqlite('database.sqlite');
 
 
 exports.addKeyword = function(projectId, keyword) {
-
     if(isAlreadyPresent(projectId, keyword)) return false;
-
     let insert = db.prepare('INSERT INTO projectKeywords VALUES (?, ?)');
     let result = insert.run([projectId, keyword]);
     return result.changes === 1;
 }
 
 exports.removeKeyword = function(projectId, keyword) {
-
     let result = db.prepare('DELETE FROM projectKeywords WHERE projectId=? AND keyword=?').run([projectId, keyword]);
     return result.changes === 1;
 }
 
 function isAlreadyPresent(projectId, keyword) {
-    
     let check = db.prepare('SELECT keyword FROM projectKeywords WHERE projectId=? AND keyword=?').get([projectId, keyword]);
-
     return check !== undefined;
 }
